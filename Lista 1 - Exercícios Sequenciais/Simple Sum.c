1244586977f472343e8213aba3cad24911aa8b80
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-04-03 12:36:35 -0300

    Create Simple Sum.c

---
+++ Lista 1 - Exercícios Sequenciais/Simple Sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(){
+    
+    int a, b, resultado;
+
+    scanf("%d %d", &a, &b);
+
+    resultado = a + b;
+    printf("SOMA = %d\n", resultado);
+
+
+
+    return 0;
+}

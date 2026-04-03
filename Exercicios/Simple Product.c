ee1c1ef507a847f560bfb98638f3d550008758ef
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-04-03 12:29:52 -0300

    Create Simple Product.c

---
+++ Exercicios/Simple Product.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    
+    int a, b, resultado;
+
+    scanf("%d %d", &a, &b);
+
+    resultado = a * b;
+    printf("PROD = %d\n", resultado);
+
+    return 0;
+}

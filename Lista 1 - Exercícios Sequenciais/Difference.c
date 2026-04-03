a90a7e784a6f273c3ba9f9ab0a836a7a9819f7d2
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-04-03 12:33:46 -0300

    Create Difference.c

---
+++ Lista 1 - Exercícios Sequenciais/Difference.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, C, D, resultado;
+
+    scanf("%d %d %d %d", &A, &B, &C, &D);
+    
+    result = (A * B) - (C * D);
+  
+    printf("DIFERENCA = %d\n", resultado);
+
+    return 0;
+}

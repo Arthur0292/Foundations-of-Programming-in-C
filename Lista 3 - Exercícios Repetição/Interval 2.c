f2c77ee15cddd6780d7f92126b92f2e8232fac82
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-04-14 23:07:43 -0300

    Create Interval 2.c

---
+++ Lista 3 - Exercícios Repetição/Interval 2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int x, n, in = 0, out = 0;
+
+    scanf("%d", &x);
+    for(int i = 0; i<x; i++){
+        scanf("%d", &n);
+        if((n >= 10) && (n <= 20)){
+            in++;
+        }else{
+            out++;
+        }
+    }
+        
+    
+
+        printf("%d in\n", in);
+        printf("%d out\n", out);
+
+
+
+    return 0;
+}

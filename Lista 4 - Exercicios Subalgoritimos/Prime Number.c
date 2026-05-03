64bffdc5572f4d990ed5e68df2d8767dd2d5e771
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-05-03 19:26:48 -0300

    Add prime number checker in Prime Number.c
    Implement prime number checking and display results.

---
+++ Lista 4 - Exercicios Subalgoritimos/Prime Number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int primo(int n){
+    int i;
+    int m = (int) sqrt(n);
+    for(i = 2; i <= m; i++){
+        if(n%i == 0){
+            return 0;
+        }
+    }
+    return 1;
+}
+ 
+int main(){
+    int i;  
+    for(i = 2; i <= 100; i++){
+        if(primo(i) == 1)
+            printf("%d eh primo\n", i);
+        else
+            printf("%d nao eh primo\n", i);
+    }
+    return 0;
+}   

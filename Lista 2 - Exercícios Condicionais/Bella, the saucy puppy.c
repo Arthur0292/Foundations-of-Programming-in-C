a0e113a0d231b14e530857ab710803aa9a1e730c
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-06-04 12:55:10 -0300

    Add Bella, the saucy puppy C program

---
+++ Lista 2 - Exercícios Condicionais/Bella, the saucy puppy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+   int n;
+   scanf("%d", &n);
+
+   if(n > 3){
+    printf("vai ficar sem o biscoito\n");
+   }else{
+    printf("vai ganhar o biscoito\n");
+   }
+
+    return 0;
+}

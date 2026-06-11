7ba38876fe3e0bbdbd655365a17a666e698c5243
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-06-11 10:21:00 -0300

    Create Closing Tabs.c

---
+++ Lista 3 - Exercícios Repetição/Closing Tabs.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+   int n, m;
+   char acao[20];
+
+   scanf("%d %d", &n, &m);
+
+   for(int i = 0; i<m; i++){
+        scanf("%s", acao);
+
+        if(strcmp(acao, "fechou") == 0){
+            n += 1;
+        }else{
+            n--;
+        }
+   }
+
+   printf("%d\n", n);
+
+
+
+    return 0;
+}

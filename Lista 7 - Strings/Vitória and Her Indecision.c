0a1fa5bd15144e06cdc63372ee16f7bbfd097585
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-06-13 14:28:34 -0300

    Create Vitória and Her Indecision.c

---
+++ Lista 7 - Strings/Vitória and Her Indecision.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int n;
+    char curso[30];
+
+    for(int i = 0; i<n; i++){
+        scanf("%s", curso);
+    }
+
+    printf("Ciencia da Computacao\n");
+    
+
+    return 0;
+}

2f58d4502017daadc74bfd3052057ceec5402c32
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-06-13 14:09:15 -0300

    Create The Pronalância Puzzle.c

---
+++ Lista 7 - Strings/The Pronalância Puzzle.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    int tamanho, cont = 0;
+    char numero[1001];
+    char numeroIn[1001];
+
+    scanf("%s", numero);
+
+    tamanho = strlen(numero);
+
+    for(int i = tamanho - 1; i >= 0; i--){
+        numeroIn[cont] = numero[i];
+        cont++;
+    }
+
+    numeroIn[cont] = '\0';
+
+    printf("%s\n", numeroIn);
+
+
+    
+    
+
+    
+
+
+    return 0;
+}

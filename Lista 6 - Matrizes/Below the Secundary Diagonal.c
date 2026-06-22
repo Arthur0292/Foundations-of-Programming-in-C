36c320bdedb500ff38c5abba09d72711ad72d3b1
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-06-22 19:43:30 -0300

    Create Below the Secundary Diagonal.c

---
+++ Lista 6 - Matrizes/Below the Secundary Diagonal.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(){
+    
+    int cont = 11;
+    double matriz[12][12];
+    char operacao;
+    double soma = 0, media = 0;
+    
+    scanf("%c", &operacao);
+
+    for(int i = 0; i<12; i++){
+        for(int j = 0; j<12; j++){
+            scanf("%lf", &matriz[i][j]);
+        }
+    }
+
+    for(int i = 1; i<12; i++){
+        for(int j = 11; j>=cont; j--){
+            soma+= matriz[j][i];
+        }
+        cont--;
+    }
+
+
+    if(operacao == 'S'){
+        printf("%.1lf\n", soma);
+    }else{
+        media = soma/66.0;
+        printf("%.1lf\n", media);
+    }
+
+    
+
+
+
+
+
+    return 0;
+}

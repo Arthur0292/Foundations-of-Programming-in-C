3cf745324fe64bbbd596139579b20f67f8f730b9
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-06-22 19:36:46 -0300

    Create Left Area.c

---
+++ Lista 6 - Matrizes/Left Area.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(){
+    
+    int cont = 1;
+    double matriz[12][12];
+    char operacao;
+    double soma = 0, media = 0;
+    
+    scanf("%c", &operacao);
+
+    for(int i = 0; i<12; i++){
+        for(int j = 0; j<12; j++){
+            scanf("%lf", &matriz[i][j]);
+        }
+    }
+
+    for(int i = 1; i<6; i++){
+        for(int j = 0; j<cont; j++){
+            soma+= matriz[i][j];
+        }
+        cont++;
+    }
+    
+    cont = 4;
+
+    for(int i = 6; i<11; i++){
+        for(int j = cont; j>=0; j--){
+            soma += matriz[i][j];
+        }
+        cont--;
+    }
+
+    if(operacao == 'S'){
+        printf("%.1lf\n", soma);
+    }else{
+        media = soma/30.0;
+        printf("%.1lf", media);
+    }
+
+    
+
+
+
+
+
+    return 0;
+}

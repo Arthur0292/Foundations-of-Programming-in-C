a2773809252bec70f5275f6e7387d158138ea8b0
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-06-28 15:41:23 -0300

    Update Letras.c

--- P3/Letras.c
+++ P3/Letras.c
@@ -1,43 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
+int main() {
 
-    int tamanho, atual = 0;
-    double cont = 0, palavra = 0;
-    double porcetagem;
+    char frase[10000];
+    int aux = 0;
+    double palavra = 0;
+    double cont = 0;
     char letra;
-    char frase[1001];
 
     scanf("%c", &letra);
     scanf(" %[^\n]", frase);
 
-    tamanho = strlen(frase);
-
-    for(int i = 0; i<tamanho; i++){
-        if(frase[i] == ' '){
-            atual = i;
-        }else if(frase[i] >= 'a' && frase[i] <= 'z' && frase[i + 1] == ' ' || frase[i + 1] == '\0'){
+    int tamanho = strlen(frase);
+    for(int i = 0; i<tamanho; i ++){
+        if(frase[i] == ' ' || i == tamanho - 1 && frase[i - 1] >= 'a' && frase[i - 1] <= 'z'){
             palavra++;
-            for(int j = i; j>atual; j--){
+            for(int j = aux; j<i; j++){
                 if(frase[j] == letra){
                     cont++;
                     break;
                 }
             }
+            aux = i + 1;
         }
     }
 
-    double resultado = (cont * 100)/palavra;
-
-    printf("%.1lf\n", resultado);
-
-
-
-
-
-
-
+    double r = (cont * 100)/palavra;
+    printf("%.1lf\n", r);
 
     return 0;
 }

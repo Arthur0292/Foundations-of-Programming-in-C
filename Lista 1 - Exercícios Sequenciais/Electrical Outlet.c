5038d27eb97a4d79b9d044ad9b83a5933b28c57d
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-06-29 15:54:39 -0300

    Create Electrical Outlet.c

---
+++ Lista 1 - Exercícios Sequenciais/Electrical Outlet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int a, b, c, d, tomadas;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    tomadas = a + b + c + d - 3;
+
+    printf("%d\n", tomadas);
+
+    return 0;
+}Electrical Outlet

d63cafff1009ca5623b8793e6e2bfa2265ec8e73
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-07-01 20:07:17 -0300

    Create Lexical.c

---
+++ PS/Lexical.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+   char a[1000];
+   char b[1000];
+
+   scanf("%s %s", a, b);
+
+    if(strcmp(a, b) <= 0){
+        printf("%s\n", a);
+        printf("%s\n", b);
+    }else{
+        printf("%s\n", b);
+        printf("%s\n", a);
+    }
+
+    return 0;
+}

0af1defa6537463429d5a3e29be3574889a0cde9
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-07-01 23:03:48 -0300

    Create Garçom.c

---
+++ PS/Garçom.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, latas, copos, soma = 0;
+    scanf("%d", &n);
+
+    for(int i = 0; i<n; i++){
+    scanf("%d %d", &latas, &copos);
+
+    if(latas > copos){
+        soma += copos;
+    }
+
+    }
+    printf("%d\n", soma);
+    return 0; 
+}

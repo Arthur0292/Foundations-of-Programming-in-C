61512643b829f948c7b6359cf53bd7719a3d8808
Arthur0292 <arthurvitordasilva01@gmail.com>
2026-07-15 19:17:14 -0300

    Create Long Live Science!.c

---
+++ Lista 7 - Strings/Long Live Science!.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char frase[1000];
+
+    scanf("%s", frase);
+
+    printf("Nao se refuta ciencia com opiniao\n");
+
+
+    return 0;
+}
